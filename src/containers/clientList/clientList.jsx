181c4b41ea9900974654ce85faf60d259fedd265
D3T3F <davidtx.fz@gmail.com>
2023-11-23 21:14:59 -0300

    ajustes no css da tabela e criado um botão de delete

--- src/containers/clientList/clientList.jsx
+++ src/containers/clientList/clientList.jsx
@@ -1,15 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Navbar } from "../../components/navbar/navbar";
+import lixeira from "../../assets/imagens/icons/icons8-lixeira.svg";
 import "./clientList.css";
 
 const Linha = (props) => {
-  console.log(props);
+  function deleteCliente(id) {
+    fetch(`http://localhost:3000/deleteCliente&id=${id}`).then(
+      window.location.reload()
+    );
+  }
+
   return (
     <tr>
+      <td className="lixeira">
+        <img
+          src={lixeira}
+          alt="excluir"
+          onClick={deleteCliente(props.cliente.id)}
+        />
+      </td>
       <td className="nome">{props.cliente.nome}</td>
-      <td>{props.cliente.telefone}</td>
-      <td>{props.cliente.cep}</td>
-      <td>R$ {props.cliente.mensalidade}</td>
+      <td>
+        {props.cliente.telefone
+          .replace(/\D/g, "")
+          .replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3")}
+      </td>
+      <td>
+        {props.cliente.cep
+          .replace(/\D/g, "")
+          .replace(/^(\d{5})(\d{3})$/, "$1-$2")}
+      </td>
+      <td>
+        {parseFloat(props.cliente.mensalidade).toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </td>
     </tr>
   );
 };
@@ -27,22 +53,29 @@ export const ClientList = () => {
     <Navbar>
       <div className="area">
         <div className="table-title">CLIENTES</div>
-        <table className="tabela">
-          <thead className="colunas">
-            <tr>
-              <th className="nome">Nome</th>
-              <th>Telefone</th>
-              <th>CEP</th>
-              <th>Mensalidade</th>
-            </tr>
-          </thead>
-          <tbody className="linhas">
-            {clientes[0] &&
-              clientes[0].map((cliente) => {
-                return <Linha key={cliente.id} cliente={cliente} />;
-              })}
-          </tbody>
-        </table>
+        <div className="tabela">
+          <table className="tabela" cellSpacing="0">
+            <thead className="colunas">
+              <tr>
+                <th className="lixeira"></th>
+                <th className="nome">Nome</th>
+                <th>Telefone</th>
+                <th>CEP</th>
+                <th>Mensalidade</th>
+              </tr>
+            </thead>
+            <tbody className="linhas">
+              {clientes[0] &&
+                clientes[0].map((cliente) => {
+                  return <Linha key={cliente.id} cliente={cliente} />;
+                })}
+            </tbody>
+          </table>
+        </div>
+        <div className="botoes">
+          <div className="btn novo">NOVO</div>
+          <div className="btn salvar">SALVAR</div>
+        </div>
       </div>
     </Navbar>
   );
